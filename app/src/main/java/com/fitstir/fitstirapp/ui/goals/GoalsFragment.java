883c90b577b6a5fe667b7dfc5f2d14927b092bb4
shastye <clubbsm.53787@gmail.com>
2023-06-23 16:57:17 -0400

    Fixed initial refactoring error

--- app/src/main/java/com/fitstir/fitstirapp/ui/goals/GoalsFragment.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/goals/GoalsFragment.java
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.fitstir.fitstirapp.databinding.FragmentHealthBinding;
+import com.fitstir.fitstirapp.databinding.FragmentGoalsBinding;
 
 public class GoalsFragment extends Fragment {
 
-    private FragmentHealthBinding binding;
+    private FragmentGoalsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GoalsViewModel goalsViewModel =
                 new ViewModelProvider(this).get(GoalsViewModel.class);
 
-        binding = FragmentHealthBinding.inflate(inflater, container, false);
+        binding = FragmentGoalsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHealth;
+        final TextView textView = binding.textGoals;
         goalsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
