a4d232241af97e5bb82437d73d2ee1db0f004e5d
shastye <clubbsm.53787@gmail.com>
2023-06-23 16:26:08 -0400

    Refactored Navigation from "Home, Dashboard, Notifications" to "Workouts, Health, Goals, Explore"

---
+++ app/src/main/java/com/fitstir/fitstirapp/ui/goals/GoalsViewModel.java
@@ -0,0 +1,19 @@
+package com.fitstir.fitstirapp.ui.goals;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GoalsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GoalsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is goals fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
