7a11aeb54f2d8ab7ddeda60a548ed496168e01aa
shastye <shastye.7x@gmail.com>
2023-06-24 13:34:48 -0400

    Added generic activity for log in/sign up activity

---
+++ app/src/main/java/com/fitstir/fitstirapp/ConnectActivity.java
@@ -0,0 +1,38 @@
+package com.fitstir.fitstirapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.fitstir.fitstirapp.R;
+import com.fitstir.fitstirapp.databinding.ActivityConnectBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class ConnectActivity extends AppCompatActivity {
+
+    private ActivityConnectBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityConnectBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavigationView navView = findViewById(R.id.nav_view_connect);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_initial)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_connect);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navViewConnect, navController);
+    }
+
+}
