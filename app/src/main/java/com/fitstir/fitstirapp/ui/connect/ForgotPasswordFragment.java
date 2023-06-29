36d82b9e84f8060826887745b919fdd6f13cdf3c
Arrington Perry <perryceo21@hotmail.com>
2023-06-28 20:32:55 -0400

    Added navigation to forgot password buttons

--- app/src/main/java/com/fitstir/fitstirapp/ui/connect/ForgotPasswordFragment.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/connect/ForgotPasswordFragment.java
@@ -1,26 +1,60 @@
 package com.fitstir.fitstirapp.ui.connect;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.fitstir.fitstirapp.MainActivity;
 import com.fitstir.fitstirapp.R;
+import com.fitstir.fitstirapp.databinding.FragmentForgotPasswordBinding;
+import com.fitstir.fitstirapp.databinding.FragmentSignUpBinding;
+
+import java.util.Objects;
 
 public class ForgotPasswordFragment extends Fragment {
 
-
+    private FragmentForgotPasswordBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ConnectViewModel connectViewModel =
+                new ViewModelProvider(this).get(ConnectViewModel.class);
 
+        binding = FragmentForgotPasswordBinding.inflate(inflater,container, false);
+        View root = binding.getRoot();
+
+        Objects.requireNonNull(((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).hide();
+
+        Button submitButton = root.findViewById(R.id.submit_password_forgot_button);
+        submitButton.setOnClickListener(v->{
+            //TODO: Reset Password
+
+            //After submit return to login
+            Navigation.findNavController(v).navigate(R.id.action_forgotPasswordFragment_to_navigation_log_in);
+        });
+
+        Button cancelButton = root.findViewById(R.id.cancel_password_forgot_button);
+        cancelButton.setOnClickListener(v->{
+          // cancelled password reset request
+            Navigation.findNavController(v).navigate(R.id.action_forgotPasswordFragment_to_navigation_log_in);
+        });
+        return root;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
