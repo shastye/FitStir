d1df114316e8bf70bfc2d7d0f915c35eae2b113e
shastye <clubbsm.53787@gmail.com>
2023-07-03 17:45:23 -0400

    Added UI design to drop down boxes; not perfect, but that's the best I could do

--- app/src/main/java/com/fitstir/fitstirapp/ui/settings/EditSettingsFragment.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/settings/EditSettingsFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.fitstir.fitstirapp.R;
 import com.fitstir.fitstirapp.databinding.FragmentEditSettingsBinding;
+import com.fitstir.fitstirapp.ui.utility.Methods;
 
 public class EditSettingsFragment extends Fragment {
 
@@ -29,6 +34,22 @@ public class EditSettingsFragment extends Fragment {
 
         // Add additions here
 
+        Spinner themeSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.themeID_spinner, root.getResources().getStringArray(R.array.theme_array));
+        Spinner rangeSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.rangeID_spinner, root.getResources().getStringArray(R.array.range_array));
+        Spinner intervalSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.intervalID_spinner, root.getResources().getStringArray(R.array.interval_array));
+        Spinner unitsSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.unitsID_spinner, root.getResources().getStringArray(R.array.unit_array));
+
+        CardView saveButton = root.findViewById(R.id.savebutton_cardView_edit);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (SettingsViewModel.themeID != themeSpinner.getSelectedItemPosition()) {
+                      // TODO: change theme; no point in doing this if it doesn't change
+
+
+                }
+            }
+        });
 
 
         // End
@@ -39,6 +60,9 @@ public class EditSettingsFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        // TODO: save to database
+
         binding = null;
     }
 }
--- app/src/main/java/com/fitstir/fitstirapp/ui/utility/Methods.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/utility/Methods.java
@@ -1,7 +1,12 @@
 package com.fitstir.fitstirapp.ui.utility;
 
 public class Methods {
-    public static void doSomething() {
+    public static Spinner getSpinnerWithAdapter(Activity _activity, View _root, int _spinnerID, String[] _spinnerOptions) {
+        Spinner spinner = (Spinner) _root.findViewById(_spinnerID);
+        ArrayAdapter<CharSequence> langAdapter = new ArrayAdapter<CharSequence>(_activity, R.layout.spinner_text, _spinnerOptions);
+        langAdapter.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner.setAdapter(langAdapter);
 
+        return spinner;
     }
 }
