8f54eb81271679ca6513c7a29158dbd8c7b7b1c9
shastye <clubbsm.53787@gmail.com>
2023-07-03 22:11:05 -0400

    Added saving feature between EditSettingsFragment and SettingsViewModel

--- app/src/main/java/com/fitstir/fitstirapp/ui/settings/EditSettingsFragment.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/settings/EditSettingsFragment.java
@@ -37,13 +37,28 @@ public class EditSettingsFragment extends Fragment {
         Spinner themeSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.themeID_spinner, root.getResources().getStringArray(R.array.theme_array));
         Spinner rangeSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.rangeID_spinner, root.getResources().getStringArray(R.array.range_array));
         Spinner intervalSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.intervalID_spinner, root.getResources().getStringArray(R.array.interval_array));
-        Spinner unitsSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.unitsID_spinner, root.getResources().getStringArray(R.array.unit_array));
+        Spinner unitSpinner = Methods.getSpinnerWithAdapter(getActivity(), root, R.id.unitsID_spinner, root.getResources().getStringArray(R.array.unit_array));
+
+        themeSpinner.setSelection(SettingsViewModel.themeID);
+        rangeSpinner.setSelection(SettingsViewModel.rangeID);
+        intervalSpinner.setSelection(SettingsViewModel.intervalID);
+        unitSpinner.setSelection(SettingsViewModel.unitID);
 
         CardView saveButton = root.findViewById(R.id.savebutton_cardView_settings_edit);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO: edit the stuff
+
+                int theme = themeSpinner.getSelectedItemPosition();
+                int range = rangeSpinner.getSelectedItemPosition();
+                int interval = intervalSpinner.getSelectedItemPosition();
+                int unit = unitSpinner.getSelectedItemPosition();
+
+                SettingsViewModel.themeID = theme;
+                SettingsViewModel.rangeID = range;
+                SettingsViewModel.intervalID = interval;
+                SettingsViewModel.unitID = unit;
 
                 Navigation.findNavController(root).navigate(R.id.action_navigation_edit_settings_to_navigation_settings);
             }
