0d93fa3cfdb8c5b53315eece9ba7d451f30b5b04
shastye <clubbsm.53787@gmail.com>
2023-07-18 20:04:42 -0400

    Added ViewGoalFragment to navigation with binding

---
+++ app/src/main/java/com/fitstir/fitstirapp/ui/goals/ViewGoalFragment.java
@@ -0,0 +1,237 @@
+package com.fitstir.fitstirapp.ui.goals;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.util.Pair;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.fitstir.fitstirapp.databinding.FragmentViewGoalBinding;
+import com.fitstir.fitstirapp.ui.utility.Methods;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ViewGoalFragment extends Fragment {
+
+    private FragmentViewGoalBinding binding;
+    private Goal goal;
+
+    private TextView nameTextView, typeTextView, valueTextView, idTextView;
+    private GraphView graphView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        GoalsViewModel goalsViewModel =
+                new ViewModelProvider(this).get(GoalsViewModel.class);
+
+        binding = FragmentViewGoalBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //final TextView textView = binding.textGoals;
+        //goalsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // Addition Text Here
+
+        this.nameTextView = binding.viewGoalNameLabel;
+        this.typeTextView = binding.viewGoalTypeLabel;
+        this.valueTextView = binding.viewGoalValueLabel;
+        this.idTextView = binding.viewGoalIdLabel;
+        this.graphView = binding.viewGoalGridGraph;
+
+        // End
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void bind(Goal goal) {
+        this.goal = goal;
+
+
+
+        this.nameTextView.setText(this.goal.getName());
+        this.typeTextView.setText(this.goal.getType());
+        String valueText = String.valueOf(this.goal.getValue()) + " " + this.goal.getUnit();
+        this.valueTextView.setText(valueText);
+        this.idTextView.setText(this.goal.getID());
+
+
+
+        int colorPrimaryVariant = Methods.getThemeAttributeColor(com.google.android.material.R.attr.colorPrimaryVariant, requireContext());
+        int colorOnPrimary = Methods.getThemeAttributeColor(com.google.android.material.R.attr.colorOnPrimary, requireContext());
+        int colorSecondary = Methods.getThemeAttributeColor(com.google.android.material.R.attr.colorSecondary, requireContext());
+
+        int range = 30; // TODO: get from settings in days
+
+        Date startDate = this.goal.getMinDate();
+        Date endDate = this.goal.getMaxDate();
+        Calendar startCal = Calendar.getInstance();
+        Calendar endCal = Calendar.getInstance();
+
+        if (startDate != null && endDate != null) {
+            startCal.setTime(startDate);
+            endCal.setTime(endDate);
+        } else if (endDate != null) {
+            startCal.setTime(endDate);
+            endCal.setTime(endDate);
+        }
+
+        Calendar rangeStart = Calendar.getInstance();
+        rangeStart.setTime(endCal.getTime());
+        rangeStart.add(Calendar.DATE, -1 * range);
+        if (startCal.getTime().after(rangeStart.getTime())) {
+            rangeStart.setTime(startCal.getTime());
+        }
+
+        Calendar windowWidth = Calendar.getInstance();
+        windowWidth.setTime(rangeStart.getTime());
+        windowWidth.add(Calendar.DATE, 15);
+
+        ArrayList<Pair<Date, Double>> inRange = new ArrayList<>();
+        for (int i = 0; i < this.goal.getData().size(); i++) {
+            if (!this.goal.getData().get(i).first.after(endCal.getTime()) && !this.goal.getData().get(i).first.before(rangeStart.getTime())) {
+                inRange.add(new Pair<>(this.goal.getData().get(i).first, this.goal.getData().get(i).second));
+            }
+        }
+
+        DataPoint[] dataPoints = new DataPoint[inRange.size()];
+        for (int i = 0; i < inRange.size(); i++) {
+            dataPoints[i] = new DataPoint(inRange.get(i).first, inRange.get(i).second);
+        }
+        LineGraphSeries<DataPoint> dataLine = new LineGraphSeries<>(dataPoints);
+        dataLine.setColor(colorPrimaryVariant);
+        dataLine.setThickness(15);
+        dataLine.setBackgroundColor(colorOnPrimary);
+        dataLine.setDrawBackground(true);
+        this.graphView.addSeries(dataLine);
+
+        DataPoint[] valuePoints = new DataPoint[] {
+                new DataPoint(dataLine.getLowestValueX(), this.goal.getValue()),
+                new DataPoint(endCal.getTime(), this.goal.getValue())
+        };
+        LineGraphSeries<DataPoint> valueLine = new LineGraphSeries<>(valuePoints);
+        valueLine.setColor(colorSecondary);
+        this.graphView.addSeries(valueLine);
+
+        this.graphView.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);
+
+        this.graphView.getGridLabelRenderer().setVerticalAxisTitle(this.goal.getUnit());
+        this.graphView.getGridLabelRenderer().setVerticalAxisTitleColor(colorOnPrimary);
+
+        this.graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                String toReturn = "";
+
+                if (isValueX) {
+                    Date dateVal = new Date((long) value);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(dateVal);
+                    int month = calendar.get(Calendar.MONTH);
+                    int day = calendar.get(Calendar.DATE);
+
+                    if (day == 1) {
+                        switch (month) {
+                            case Calendar.JANUARY:
+                                toReturn = "Jan";
+                                break;
+                            case Calendar.FEBRUARY:
+                                toReturn = "Feb";
+                                break;
+                            case Calendar.MARCH:
+                                toReturn = "Mar";
+                                break;
+                            case Calendar.APRIL:
+                                toReturn = "Apr";
+                                break;
+                            case Calendar.MAY:
+                                toReturn = "May";
+                                break;
+                            case Calendar.JUNE:
+                                toReturn = "Jun";
+                                break;
+                            case Calendar.JULY:
+                                toReturn = "Jul";
+                                break;
+                            case Calendar.AUGUST:
+                                toReturn = "Aug";
+                                break;
+                            case Calendar.SEPTEMBER:
+                                toReturn = "Sep";
+                                break;
+                            case Calendar.OCTOBER:
+                                toReturn = "Oct";
+                                break;
+                            case Calendar.NOVEMBER:
+                                toReturn = "Nov";
+                                break;
+                            case Calendar.DECEMBER:
+                                toReturn = "Dec";
+                                break;
+                            default:
+                                toReturn = "oops";
+                                break;
+                        }
+                    } else {
+                        toReturn = "";
+
+                        if (day % 2 == 1) {
+
+                            if (day < 10) {
+                                toReturn = "0";
+                            }
+
+                            toReturn += String.valueOf(day);
+                        }
+                    }
+                } else {
+                    if ((((int) value) % 4) == 0) {
+                        toReturn = String.valueOf((int) value);
+                    } else {
+                        toReturn = "";
+                    }
+                }
+
+                return toReturn;
+            }
+        });
+        this.graphView.getGridLabelRenderer().setNumHorizontalLabels(18);
+        this.graphView.getGridLabelRenderer().setHumanRounding(false, true);
+        this.graphView.getGridLabelRenderer().setHorizontalLabelsColor(colorOnPrimary);
+
+        this.graphView.getViewport().setYAxisBoundsManual(true);
+        double minVal = dataLine.getLowestValueY();
+        double maxVal = dataLine.getHighestValueY();
+        if (this.goal.getValue() > maxVal) {
+            maxVal = this.goal.getValue();
+        } else if (this.goal.getValue() < minVal) {
+            minVal = this.goal.getValue();
+        }
+        this.graphView.getViewport().setMinY(minVal - 2);
+        this.graphView.getViewport().setMaxY(maxVal + 2);
+
+        this.graphView.getViewport().setXAxisBoundsManual(true);
+        rangeStart.add(Calendar.DATE, -2);
+        this.graphView.getViewport().setMinX(rangeStart.getTime().getTime());
+        this.graphView.getViewport().setMaxX(windowWidth.getTime().getTime());
+
+        this.graphView.getViewport().setScrollable(true);
+        this.graphView.getViewport().scrollToEnd();
+    }
+}
