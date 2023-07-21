715a21dac95c42fce2dac9973bda232f849c2a36
shastye <clubbsm.53787@gmail.com>
2023-07-21 19:54:32 -0400

    Moved "getGenericGoal(int)" from Methods to GoalsViewModel

--- app/src/main/java/com/fitstir/fitstirapp/ui/goals/GoalsViewModel.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/goals/GoalsViewModel.java
@@ -4,20 +4,18 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.fitstir.fitstirapp.databinding.FragmentViewGoalBinding;
-import com.fitstir.fitstirapp.ui.utility.Methods;
+import com.fitstir.fitstirapp.ui.utility.Tags;
 
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
 
 public class GoalsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
     private final MutableLiveData<Goal> clickedGoal = new MutableLiveData<>(new Goal());
     private final MutableLiveData<ArrayList<Goal>> goals = new MutableLiveData<>(new ArrayList<Goal>() {{
-        add(Methods.getGenericGoal(0));
-        add(Methods.getGenericGoal(1));
+        add(getGenericGoal(0));
+        add(getGenericGoal(1));
     }});
     private final MutableLiveData<Integer> goalRange = new MutableLiveData<Integer>(30); // TODO: Get from settings in days
 
@@ -45,4 +43,84 @@ public class GoalsViewModel extends ViewModel {
         goals.setValue(temp);
     }
     public void setGoalRange(int range) { goalRange.setValue(range); }
+
+    private Goal getGenericGoal(int goalNum) {
+        Goal g;
+        Calendar calendar = Calendar.getInstance();
+
+        switch(goalNum) {
+            case 1:
+                g = new Goal("Test1", Tags.Workout_Type.RUN_CLUB_DISTANCE, 16);
+
+                calendar.set(Calendar.MONTH, Calendar.MAY);
+                calendar.set(Calendar.DAY_OF_MONTH, 10);
+                calendar.set(Calendar.YEAR, 2023);
+                g.addData(calendar.getTime(), 1.1);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 3.6);
+                calendar.add(Calendar.DATE, 1);
+                g.addData(calendar.getTime(), 4.7);
+                calendar.add(Calendar.DATE, 5);
+                g.addData(calendar.getTime(), 7.0);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 8.9);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.5);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.7);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.0);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.9);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.2);
+                calendar.add(Calendar.DATE, 5);
+                g.addData(calendar.getTime(), 7.9);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 8.4);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 12.9);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 11.1);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 8.6);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 14.7);
+                break;
+            default:
+                g = new Goal("Test#", Tags.Workout_Type.RUN_CLUB_ENDURANCE, 14);
+
+                calendar.set(Calendar.MONTH, Calendar.JUNE);
+                calendar.set(Calendar.DAY_OF_MONTH, 1);
+                calendar.set(Calendar.YEAR, 2023);
+                g.addData(calendar.getTime(), 3.6);
+                calendar.add(Calendar.DATE, 1);
+                g.addData(calendar.getTime(), 4.7);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 8.9);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.5);
+                calendar.add(Calendar.DATE, 3);
+                g.addData(calendar.getTime(), 7.7);
+                calendar.add(Calendar.DATE, 4);
+                g.addData(calendar.getTime(), 7.0);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.9);
+                calendar.add(Calendar.DATE, 6);
+                g.addData(calendar.getTime(), 7.2);
+                calendar.add(Calendar.DATE, 5);
+                g.addData(calendar.getTime(), 7.9);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 8.4);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 12.9);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 11.1);
+                calendar.add(Calendar.DATE, 2);
+                g.addData(calendar.getTime(), 8.6);
+                break;
+        }
+
+        return g;
+    }
 }
