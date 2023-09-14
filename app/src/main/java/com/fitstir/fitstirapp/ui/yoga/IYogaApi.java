b34539c2e297da65a926319ffad26a1e47093c50
Arrington Perry <perryceo21@hotmail.com>
2023-09-14 08:43:10 -0400

    endpoint calls complete

--- app/src/main/java/com/fitstir/fitstirapp/ui/yoga/IYogaApi.java
+++ app/src/main/java/com/fitstir/fitstirapp/ui/yoga/IYogaApi.java
@@ -1,4 +1,32 @@
 package com.fitstir.fitstirapp.ui.yoga;
 
-public interface IYogaApi {
+import com.fitstir.fitstirapp.ui.yoga.models.CategoryModel;
+import com.fitstir.fitstirapp.ui.yoga.models.PoseModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface IYogaApi
+{
+    @GET("/categories")
+    Call<List<CategoryModel>> getCategories();
+    @GET("/categories?id=value")
+    Call<List<CategoryModel>> getCategoriesById();
+    @GET("/categories?name=value")
+    Call<List<CategoryModel>> getCategoriesByName();
+    @GET("/categories?id=value&level=value")
+    Call<List<CategoryModel>> getCategoriesByLevel();
+
+    @GET("/poses")
+    Call<List<PoseModel>> getPoses();
+    @GET("/poses?id=value")
+    Call<List<PoseModel>> getPosesByID();
+    @GET("/poses?name=value")
+    Call<List<PoseModel>> getPosesByName();
+    @GET("/poses?level=value")
+    Call<List<PoseModel>> getPosesByLevel();
+
+
 }
