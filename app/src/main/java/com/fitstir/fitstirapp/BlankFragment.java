d68e0ac132a52980c36c523e908aabd4363eb50b
shastye <clubbsm.53787@gmail.com>
2023-09-18 20:56:41 -0400

    Fixed merge error for Diary section

---
+++ app/src/main/java/com/fitstir/fitstirapp/BlankFragment.java
@@ -0,0 +1,33 @@
+package com.fitstir.fitstirapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.fitstir.fitstirapp.databinding.FragmentBlankBinding;
+import com.fitstir.fitstirapp.ui.health.HealthViewModel;
+
+public class BlankFragment extends Fragment {
+    private FragmentBlankBinding binding;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HealthViewModel healthViewModel =
+                new ViewModelProvider(this).get(HealthViewModel.class);
+
+        binding = FragmentBlankBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
